fd90513858d602ef78673385faf5325d6b469877
Zoli <nahoczki@rangers.uwp.edu>
2020-12-01 01:37:30 -0600

    Map theme added

--- src/components/MapsSDK.js
+++ src/components/MapsSDK.js
@@ -27,23 +27,7 @@ class GoogleMapSDK extends Component {
         console.log(this.props.token)
 
         //Get markers from API
-        // axios.patch("https://help-spring-api.herokuapp.com/api/locations/driver-locations", call , { headers: {
-        //         "Authorization": this.props.token
-        //     }})
-        //     .then(res => {
-        //         const markers = res.data
-        //         console.log(markers)
-        //         this.setState({markers})
-        //     })
-
-
-
-        /*
-        axios({
-            method: 'PATCH',
-            url: "https://help-spring-api.herokuapp.com/api/locations/driver-locations",
-            data : call,
-            headers: {
+        axios.patch("https://help-spring-api.herokuapp.com/api/locations/driver-locations", call , { headers: {
                 "Authorization": this.props.token
             }})
             .then(res => {
@@ -51,7 +35,6 @@ class GoogleMapSDK extends Component {
                 console.log(markers)
                 this.setState({markers})
             })
-         */
     }
 
     componentDidMount() {
@@ -79,6 +62,88 @@ class GoogleMapSDK extends Component {
                     bootstrapURLKeys={{ key:"AIzaSyBKekm05H0Dxmt8ZboPyNgKqZKyYJ3Zfy8"}}
                     defaultCenter={this.props.center}
                     defaultZoom={this.props.zoom}
+                    options={{
+                        styles: [
+                            { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+                            { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+                            { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+                            {
+                                featureType: "administrative.locality",
+                                elementType: "labels.text.fill",
+                                stylers: [{ color: "#d59563" }]
+                            },
+                            {
+                                featureType: "poi",
+                                elementType: "labels.text.fill",
+                                stylers: [{ color: "#d59563" }]
+                            },
+                            {
+                                featureType: "poi.park",
+                                elementType: "geometry",
+                                stylers: [{ color: "#263c3f" }]
+                            },
+                            {
+                                featureType: "poi.park",
+                                elementType: "labels.text.fill",
+                                stylers: [{ color: "#6b9a76" }]
+                            },
+                            {
+                                featureType: "road",
+                                elementType: "geometry",
+                                stylers: [{ color: "#38414e" }]
+                            },
+                            {
+                                featureType: "road",
+                                elementType: "geometry.stroke",
+                                stylers: [{ color: "#212a37" }]
+                            },
+                            {
+                                featureType: "road",
+                                elementType: "labels.text.fill",
+                                stylers: [{ color: "#9ca5b3" }]
+                            },
+                            {
+                                featureType: "road.highway",
+                                elementType: "geometry",
+                                stylers: [{ color: "#746855" }]
+                            },
+                            {
+                                featureType: "road.highway",
+                                elementType: "geometry.stroke",
+                                stylers: [{ color: "#1f2835" }]
+                            },
+                            {
+                                featureType: "road.highway",
+                                elementType: "labels.text.fill",
+                                stylers: [{ color: "#f3d19c" }]
+                            },
+                            {
+                                featureType: "transit",
+                                elementType: "geometry",
+                                stylers: [{ color: "#2f3948" }]
+                            },
+                            {
+                                featureType: "transit.station",
+                                elementType: "labels.text.fill",
+                                stylers: [{ color: "#d59563" }]
+                            },
+                            {
+                                featureType: "water",
+                                elementType: "geometry",
+                                stylers: [{ color: "#17263c" }]
+                            },
+                            {
+                                featureType: "water",
+                                elementType: "labels.text.fill",
+                                stylers: [{ color: "#515c6d" }]
+                            },
+                            {
+                                featureType: "water",
+                                elementType: "labels.text.stroke",
+                                stylers: [{ color: "#17263c" }]
+                            }
+                        ]
+                    }}
                 >
                     {Markers}
                 </GoogleMapReact>
